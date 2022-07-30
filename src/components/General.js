d3e8ddbd3a5b74fcaa41c4e1643866b3ba52400c
hoarus <hoarus@gmail.com>
2022-07-30 18:30:59 -0400

    Refactor App as class

---
+++ src/components/General.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class General extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="Overview">
+      </div>
+    );
+  }
+}
+
+export default General;
