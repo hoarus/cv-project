887ca9f5f09c78db7e5011d502aae675945ee7fd
hoarus <hoarus@gmail.com>
2022-07-30 19:27:39 -0400

    Add general section

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        Hello World
+        <h1> CV Builder </h1>
+        <General></General>
       </div>
     );
     }
--- src/components/General.js
+++ src/components/General.js
@@ -3,14 +3,88 @@ import React, { Component } from 'react';
 class General extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      occupation: '',
+      addressLine1: '',
+      addressLine2: '',
+      town: '',
+      state: '',
+      zipCode: '',
+      phone: '',
+      email: '',
+      }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState(
+      {[event.target.name]: event.target.value}
+    );  
+  }
+  handleSubmit(event) {
+    console.log(this.state);
+    event.preventDefault();
   }
 
   render(){
     return (
-      <div className="Overview">
-      </div>
+      <form onSubmit={this.handleSubmit}> 
+      <h2> General Details </h2>
+      <div className="flex-row">
+        <div>
+          <label>
+            First Name: 
+            <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} />        
+          </label>
+          <label>
+            Last Name: 
+            <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} />        
+          </label>
+          <label>
+            Occupation: 
+            <input type="text" name="occupation" value={this.state.occupation} onChange={this.handleChange} />        
+          </label>
+        </div>
+        <div>
+          <label>
+            Address Line 1: 
+            <input type="text" name="addressLine1" value={this.state.addressLine1} onChange={this.handleChange} />        
+          </label>
+          <label>
+            Address Line 2: 
+            <input type="text" name="addressLine2" value={this.state.addressLine2} onChange={this.handleChange} />        
+          </label>
+          <label>
+            Town/City: 
+            <input type="text" name="town" value={this.state.town} onChange={this.handleChange} />        
+          </label>
+          <label>
+            State: 
+            <input type="text" name="state" value={this.state.state} onChange={this.handleChange} />        
+          </label>
+          <label>
+            Zip Code: 
+            <input type="text" name="zipCode" value={this.state.zipCode} onChange={this.handleChange} />        
+          </label>
+          <label>
+            Phone Number: 
+            <input type="text" name="phone" value={this.state.phone} onChange={this.handleChange} />        
+          </label>
+          <label>
+            Email Address: 
+            <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />        
+          </label>
+        </div>
+      </div>    
+      <input className="formSubmit" type="submit" value="Submit" />
+    </form>
     );
   }
 }
 
 export default General;
+
+
