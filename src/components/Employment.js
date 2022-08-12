2529c324f4de069bd6bb64d228e2737faa6e3be9
hoarus <hoarus@gmail.com>
2022-08-12 07:53:07 -0400

    Create function to add employment entries

--- src/components/Employment.js
+++ src/components/Employment.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class Employment extends Component {
   constructor(props) {
@@ -16,20 +16,19 @@ class Employment extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.previousPage = this.previousPage.bind(this);
+    this.addEmploymentEntry = this.addEmploymentEntry.bind(this);
   }
 
   handleChange(event) {
-    let stateSnapshot = this.state
-    const newState = stateSnapshot.employment.map(obj => {
+    let employmentEntries = this.state.employment
+    const newState = employmentEntries.map(obj => {
       // 👇️ if id equals 0, update that element of array
       if(obj.id === 0) {
           return {...obj, [event.target.name]: event.target.value};
       } else {
         return obj;
       }});
-
     this.setState(newState);
-    console.log(this.state);
   };
   
   handleSubmit(event) {
@@ -41,42 +40,61 @@ class Employment extends Component {
     this.props.previousPage();
   }
 
+  addEmploymentEntry(){
+    let employmentEntries = this.state.employment
+    let entryID = employmentEntries.length;
+    let newEntry = {
+      id: entryID,
+      company: '',
+      role: '',
+      startDate: '',
+      endDate: '',
+      details: ''}
+    let newState = [...employmentEntries, newEntry];
+    this.setState(newState);
+  }
+
+
 
 
   render(){
     return (
+      <Fragment>
+        <form onSubmit={this.handleSubmit}> 
+        <h2> Employment Details </h2>
+        <div className="flex-row">
+          <div>
+            <label>
+              Company: 
+              <input type="text" name="company" value={this.state.employment.company} onChange={this.handleChange} />        
+            </label>
+            <label>
+              Role: 
+              <input type="text" name="role" value={this.state.employment.role} onChange={this.handleChange} />        
+            </label>
+            <label>
+              Start Date: 
+              <input type="date" name="startDate" value={this.state.employment.startDate} onChange={this.handleChange} />        
+            </label>
+          </div>
+          <div>
+            <label>
+              End Date: 
+              <input type="date" name="endDate" value={this.state.employment.endDate} onChange={this.handleChange} />        
+            </label>
+            <label>
+              Details: 
+              <textarea type="text" name="details" value={this.state.employment.details} onChange={this.handleChange} />        
+            </label>
+          </div>
+        </div>    
+        <input className="formSubmit" type="submit" value="Update" />
+      </form>
       <form onSubmit={this.handleSubmit}> 
-      <h2> Employment Details </h2>
-      <div className="flex-row">
-        <div>
-          <label>
-            Company: 
-            <input type="text" name="company" value={this.state.employment.company} onChange={this.handleChange} />        
-          </label>
-          <label>
-            Role: 
-            <input type="text" name="role" value={this.state.employment.role} onChange={this.handleChange} />        
-          </label>
-          <label>
-            Start Date: 
-            <input type="date" name="startDate" value={this.state.employment.startDate} onChange={this.handleChange} />        
-          </label>
-        </div>
-        <div>
-          <label>
-            End Date: 
-            <input type="date" name="endDate" value={this.state.employment.endDate} onChange={this.handleChange} />        
-          </label>
-          <label>
-            Details: 
-            <textarea type="text" name="details" value={this.state.employment.details} onChange={this.handleChange} />        
-          </label>
-        </div>
-      </div>    
-      <input className="formSubmit" type="submit" value="Next" />
-      <button onClick= {this.previousPage} className="formSubmit">Previous</button>
-      {/* Need to add a button which goes backwards */}
-    </form>
+        <button onClick= {this.previousPage} className="formSubmit">Previous</button>
+        <button onClick= {this.addEmploymentEntry} className="formSubmit">Add Additional Employment Entry</button>
+      </form>
+    </Fragment>
     );
   }
 }
